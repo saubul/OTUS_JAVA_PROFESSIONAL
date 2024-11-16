c6c47415f82e824c0cfe504fb8ba3b7132beaabc
Ruslan <miraxle@yandex.ru>
2024-11-17 00:24:12 +0300

    hw07 Инициализация hw07

---
+++ hw07/dbinit.sql
@@ -0,0 +1,1 @@
+create table users (id bigserial primary key, login varchar(255), password varchar(255), nickname varchar(255));
---
+++ hw07/src/main/java/ru/otus/dao/AbstractRepository.java
@@ -0,0 +1,71 @@
+package ru.otus.dao;
+
+import ru.otus.annotation.RepositoryField;
+import ru.otus.annotation.RepositoryIdField;
+import ru.otus.annotation.RepositoryTable;
+import ru.otus.datasource.DataSource;
+import ru.otus.exception.ORMException;
+
+import java.lang.reflect.Field;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AbstractRepository<T> {
+    private DataSource dataSource;
+    private PreparedStatement psInsert;
+    private List<Field> cachedFields;
+
+    public AbstractRepository(DataSource dataSource, Class<T> cls) {
+        this.dataSource = dataSource;
+        this.prepareInsert(cls);
+    }
+
+    public void save(T entity) {
+        try {
+            for (int i = 0; i < cachedFields.size(); i++) {
+                psInsert.setObject(i + 1, cachedFields.get(i).get(entity));
+            }
+            psInsert.executeUpdate();
+        } catch (Exception e) {
+            throw new ORMException("Что-то пошло не так при сохранении: " + entity);
+        }
+    }
+
+    private void prepareInsert(Class cls) {
+        if (!cls.isAnnotationPresent(RepositoryTable.class)) {
+            throw new ORMException("Класс не предназначен для создания репозитория, не хватает аннотации @RepositoryTable");
+        }
+        String tableName = ((RepositoryTable) cls.getAnnotation(RepositoryTable.class)).title();
+        StringBuilder query = new StringBuilder("insert into ");
+        query.append(tableName).append(" (");
+        // 'insert into users ('
+        cachedFields = Arrays.stream(cls.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(RepositoryField.class))
+                .filter(f -> !f.isAnnotationPresent(RepositoryIdField.class))
+                .collect(Collectors.toList());
+        for (Field f : cachedFields) { // TODO заменить на использование геттеров
+            f.setAccessible(true);
+        }
+        for (Field f : cachedFields) {
+            query.append(f.getName()).append(", ");
+        }
+        // 'insert into users (login, password, nickname, '
+        query.setLength(query.length() - 2);
+        query.append(") values (");
+        // 'insert into users (login, password, nickname) values ('
+        for (Field f : cachedFields) {
+            query.append("?, ");
+        }
+        query.setLength(query.length() - 2);
+        query.append(");");
+        // 'insert into users (login, password, nickname) values (?, ?, ?);'
+        try {
+            psInsert = dataSource.getConnection().prepareStatement(query.toString());
+        } catch (SQLException e) {
+            throw new ORMException("Не удалось проинициализировать репозиторий для класса " + cls.getName());
+        }
+    }
+}
