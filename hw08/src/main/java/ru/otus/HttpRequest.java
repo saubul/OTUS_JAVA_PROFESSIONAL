05c0d534cfb08889c2a2b76e2203f7a1ba1f74aa
Ruslan <miraxle@yandex.ru>
2024-12-01 02:52:53 +0300

    #hw08 Исправление ДЗ№8

--- hw08/src/main/java/ru/otus/HttpRequest.java
+++ hw08/src/main/java/ru/otus/HttpRequest.java
@@ -1,77 +1,121 @@
 package ru.otus;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
 
-    private final String method;
-    private final String uri;
-    private final Map<String, String> headers;
-    private final Map<String, String> parameters;
+    private String method;
+
+    private String uri;
+
+    private String version;
+
+    private Map<String, String> parameters;
+
+    private Map<String, String> headers;
+
     private String body;
 
-    public HttpRequest(String method, String uri, Map<String, String> headers, Map<String, String> parameters, String body) {
-        this.method = method;
-        this.uri = uri;
-        this.headers = headers;
-        this.parameters = parameters;
-        this.body = body;
+    public HttpRequest() {
+        parameters = new HashMap<>();
+        headers = new HashMap<>();
     }
 
-
     public static HttpRequest parse(InputStream inputStream) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String requestLine = reader.readLine();
+        HttpRequest request = new HttpRequest();
 
-        if (requestLine == null) {
-            throw new IOException("Empty request");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        // Чтение метода
+        int read;
+
+        for (read = inputStream.read(); read != ' ' && read != -1; read = inputStream.read()) {
+            baos.write(read);
         }
+        request.setMethod(baos.toString());
+        baos.reset();
 
-        String[] requestParts = requestLine.split(" ");
-        String method = requestParts[0];
-        String uri = requestParts[1];
+        // Чтение URI
+        for (read = inputStream.read(); read != ' ' && read != '?' && read != -1; read = inputStream.read()) {
+            baos.write(read);
+        }
+        request.setUri(baos.toString());
+        baos.reset();
 
-        Map<String, String> parameters = new HashMap<>();
-        if (uri.contains("?")) {
-            String[] uriParts = uri.split("\\?");
-            uri = uriParts[0];
+        // Чтение параметров при наличии
+        if (read == '?') {
+            Map<String, String> parameters = new HashMap<>();
+            String paramKey;
 
-            String[] paramParts = uriParts[1].split("&");
-            for (String param : paramParts) {
-                String[] keyValue = param.split("=");
-                if (keyValue.length == 2) {
-                    parameters.put(keyValue[0], keyValue[1]);
+            do {
+                for (read = inputStream.read(); read != '=' && read != -1; read = inputStream.read()) {
+                    baos.write(read);
                 }
-            }
+                paramKey = baos.toString();
+                baos.reset();
+
+                for (read = inputStream.read(); read != '&' && read != ' ' && read != -1; read = inputStream.read()) {
+                    baos.write(read);
+                }
+                parameters.put(paramKey, baos.toString());
+                baos.reset();
+            } while (read != -1 && read != ' ');
+            baos.reset();
+
+            request.setParameters(parameters);
         }
 
-        Map<String, String> headers = new HashMap<>();
-        String headerLine;
-        while ((headerLine = reader.readLine()) != null && !headerLine.isEmpty()) {
+        // Чтение версии
+        for (read = inputStream.read(); read != '\n' && read != -1; read = inputStream.read()) {
+            if (read == '\r') {
+                continue;
+            }
+            baos.write(read);
+        }
+        request.setVersion(baos.toString());
+        baos.reset();
+
+        Map<String, String> headers = request.getHeaders();
+
+        // Чтение заголовков
+        while (true) {
+            baos.reset(); // Очищаем байтовый поток
+            for (read = inputStream.read(); read != '\n' && read != -1; read = inputStream.read()) {
+                if (read == '\r') {
+                    continue;
+                }
+                baos.write(read);
+            }
+
+            String headerLine = baos.toString("UTF-8").trim();
+            if (headerLine.isEmpty()) {
+                break;
+            }
+
             String[] headerParts = headerLine.split(": ", 2);
             if (headerParts.length == 2) {
                 headers.put(headerParts[0], headerParts[1]);
             }
         }
 
-        // Считываем тело запроса
-        StringBuilder bodyBuilder = new StringBuilder();
-        // Если есть заголовок Content-Length, считываем тело
+        // Чтение тела запроса
         if (headers.containsKey("Content-Length")) {
             int contentLength = Integer.parseInt(headers.get("Content-Length"));
-            char[] bodyBuffer = new char[contentLength];
-            reader.read(bodyBuffer, 0, contentLength);
-            bodyBuilder.append(bodyBuffer);
+
+            byte[] bodyBuffer = new byte[contentLength];
+            int bytesRead = inputStream.read(bodyBuffer);
+
+            if (bytesRead != -1) {
+                request.setBody(new String(bodyBuffer, "UTF-8"));
+            }
         }
 
-        return new HttpRequest(method, uri, headers, parameters, bodyBuilder.toString());
+        return request;
     }
-
 
     public String getMethod() {
         return method;
@@ -97,6 +141,30 @@ public class HttpRequest {
         this.body = body;
     }
 
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
     @Override
     public String toString() {
         return "HttpRequest{" +
@@ -105,6 +173,7 @@ public class HttpRequest {
                 ", headers=" + headers +
                 ", parameters=" + parameters +
                 ", body='" + body + '\'' +
+                ", version='" + version + '\'' +
                 '}';
     }
 }
