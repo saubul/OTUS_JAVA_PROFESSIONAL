eadfbd7018d7cb37450d7233f2ecea3893f033cc
Ruslan <miraxle@yandex.ru>
2024-12-15 01:42:03 +0300

    #hw13 Решение ДЗ№13

---
+++ hw13/src/main/java/ru/otus/entity/Client.java
@@ -0,0 +1,44 @@
+package ru.otus.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Fetch(value = FetchMode.SUBSELECT)
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private Set<Phone> phones;
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phones=" + phones +
+                '}';
+    }
+}
